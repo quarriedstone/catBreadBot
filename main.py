068df30ab29bf7f078066a0da018c4f027d21add
quarriedstone <quarriedstone@gmail.com>
2019-12-16 01:07:34 +0300

    Initial commit: added bot

---
+++ main.py
@@ -0,0 +1,16 @@
+from telegram.ext import Updater
+from bot.CatBreadBot import CatBreadBot
+
+
+def main():
+    updater = Updater("982112851:AAHfZlRWnSsCdm58-uUfnjS9HtlMbO72l_M", use_context=True)
+
+    dp = updater.dispatcher
+    # Added conversation for QUESTION1 and QUESTION2
+    cat_bread_bot = CatBreadBot(dp)
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
